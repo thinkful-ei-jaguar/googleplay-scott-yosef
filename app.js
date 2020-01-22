183ac3e9d4cf9f74c635edc86b06d19a9726d9d7
Scott Lingner <slingner@gmail.com>
2020-01-22 09:32:23 -0800

    added sort and genres

--- app.js
+++ app.js
@@ -3,39 +3,46 @@ const morgan = require('morgan');
 
 const app =  express();
 
+//use morgan in the 'dev' setting
 app.use(morgan('dev'));
 
+//import the data from playstore
 const storeData = require('./playstore.js');
 
 app.get('/apps', (req, res) => {
+  //deconstruct req.query.sort and req.query.genres
+  const {sort, genres} = req.query;
+
   //make a copy of app data to edit
   let filteredApps = [...storeData]
   
-  //make sure Genres query has a value
-  if('Genres' in req.query && !req.query.Genres) {
-    return status(400).json({ error: "Genres must contain a value"})
-  }
+  //this is the accepted params for Genres
+  const acceptedGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
 
-  if(req.query.genre){
-    
-  }
-  
-
-  if(req.query.sort){
-    if(req.query.sort !== 'rating' && req.query.sort !== 'app'){
-      return res.status(400).json({ error: "Sort must be one of rating or app" })
+  //make sure Genres query has a value of one of the accepted params
+  if(genres) {
+    if(!acceptedGenres.includes(genres)){
+      return res.status(400).json({ error: "Genres must be one of: Action, Puzzle, Strategy, Casual, Arcade, Card "});
     }
+      //using the copied app data(you don't want to use the original imported data)filter for each app, check that the Genres key matches the req.query.genres value and assign the new array as filteredApps
+    filteredApps = filteredApps.filter(eachApp => eachApp.Genres.includes(genres))
+  }
 
+  //if req.query.sort is not equal to 'Rating' or 'App' throw an error
+  if(sort) {
+    if(sort !== 'Rating' && sort !== 'App'){
+      return res.status(400).json({ error: "Sort must be one of: 'Rating' or 'App'" });
+    }
+    //return either 1, to leave it on the right, or a -1, to reverse the order, or 0 to leave as is if identical
+    //1 | -1
+    //[]use bracket notation to handle dynamic data
     filteredApps.sort((a, b) => {
-      return a[req.query.sort] < b[req.query.sort] ? -1 : 1;
+      return a[sort] < b[sort] ? -1 : 1;
     });
   }
 
-
-
-
-
-})
+  res.json(filteredApps);
+});
 
 
 app.listen(8000, () => {
